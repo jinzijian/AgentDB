ca8290673fe94df2f88220ea8484a49526c81ac9
Alex Kim <alexkim@AlexdeMacBook-Pro.local>
2023-11-17 23:27:08 -0800

    Feat: init DB; Embedding Models
    Signed-off-by: Alex Kim <alexkim@AlexdeMacBook-Pro.local>

---
+++ vectorDB/init_db.py
@@ -0,0 +1,28 @@
+import pinecone
+api_key="aca4b14d-7412-4d01-81d9-fdb18d197b3e"
+environment="asia-southeast1-gcp"
+
+def init_vecDB(api_key, environment):
+    pinecone.init(api_key, environment)
+
+if __name__ == "__main__":
+    pinecone.init(api_key, environment="asia-southeast1-gcp")
+    # pinecone.create_index("quickstart", dimension=8, metric="euclidean")
+    pinecone.describe_index("quickstart")
+    index = pinecone.Index("quickstart")
+
+    index.upsert(
+    vectors=[
+        {"id": "A", "values": [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]},
+        {"id": "B", "values": [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]},
+        {"id": "C", "values": [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]},
+        {"id": "D", "values": [0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4]},
+        {"id": "E", "values": [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]}
+    ]
+    )
+    print(index.query(
+    vector=[0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
+    top_k=3,
+    include_values=True
+    ))
+    pinecone.delete_index("quickstart")
---
+++ vectorDB/init_emb.py
@@ -0,0 +1,22 @@
+import langchain
+from langchain.embeddings import OpenAIEmbeddings
+key = "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"
+embeddings_model = OpenAIEmbeddings(openai_api_key=key)
+
+def get_embeddings_model(api_key):
+    embeddings_model = OpenAIEmbeddings(openai_api_key=api_key)
+    return embeddings_model
+if __name__=="__main__":
+    emb_model = get_embeddings_model(key)
+    embeddings = emb_model.embed_documents(
+    [
+        "Hi there!",
+        "Oh, hello!",
+        "What's your name?",
+        "My friends call me World",
+        "Hello World!"
+    ]
+    )
+    print(len(embeddings), len(embeddings[0]))
+    embedded_query = embeddings_model.embed_query("What was the name mentioned in the conversation?")
+    print(embedded_query[:5])
